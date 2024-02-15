d9146111a6cacd698fc8bf6904e3c7c7f928e6bb
Jagan <110019353+jaganjavid@users.noreply.github.com>
2024-02-15 21:16:06 +0530

    Is a first commit

---
+++ my-first-batch/javascript/2-data-types.js
@@ -0,0 +1,39 @@
+
+
+// Primitive Type
+
+// String
+const name = "Jagan";
+
+// Number
+const age = 26;
+
+// Boolean
+const hasKids = true;
+
+// Null
+// const shootingGun = null;
+
+// Undefined
+let undef;
+
+// console.log(shootingGun);
+// console.log(typeof shootingGun);
+
+
+// Reference Type === object
+
+const myArr = ["Jagan", "Javid", 26, true, null, undefined];
+
+// Key & Value or Name & Value
+
+const car = {
+    name: "BWM",
+    color: "Blue",
+    model: "5 series",
+    year: 2023,
+}
+
+
+console.log(car);
+console.log(typeof car);
---
+++ my-first-batch/javascript/app.js
@@ -0,0 +1,5 @@
+
+
+console.log("Javid"); // ***
+console.error("failed");
+console.warn("failed");
